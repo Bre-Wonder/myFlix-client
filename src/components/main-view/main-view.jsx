4bd69d627a078d996d7b330af8cfc8c73fdf48c3
Bre-Wonder <117392929+Bre-Wonder@users.noreply.github.com>
2023-07-06 20:40:33 -0700

    imported components, added array of movies

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -1,2 +1,19 @@
 import { useState } from "React";
 import { MovieCard } from "../movie-card/movie-card";
+import { MovieView } from "../movie-view/movie-view";
+
+export const MainView = () => {
+  const [movies, setMovies] = useState ([
+      {
+        {/* COME BACK TO THIS!! */}
+      },
+      {
+        {/* COME BACK TO THIS!! */}
+      },
+      {
+        {/* COME BACK TO THIS!! */}
+      }
+  ]);
+
+  const [selectedMovie, setSelectedMovie] = useState(null);
+}
