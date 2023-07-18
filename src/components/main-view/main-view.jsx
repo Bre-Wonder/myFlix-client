0a0121a4557d9a49452624fd568c80afac5b180b
Bre-Wonder <117392929+Bre-Wonder@users.noreply.github.com>
2023-07-17 20:38:03 -0700

    added useEffect took to pull api info

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { MovieCard } from "../movie-card/movie-card";
 import { MovieView } from "../movie-view/movie-view";
 
@@ -6,6 +6,22 @@ export const MainView = () => {
   const [movies, setMovies] = useState ([]);
 
   const [selectedMovie, setSelectedMovie] = useState(null);
+  useEffect(() => {
+    fetch("https://bre-wonder-cinema-app-8704977a1a65.herokuapp.com/")
+      .then((response) => response.json())
+      .then((data) => {
+        const moviesFromApi = data.docs.map((doc) => {
+          return {
+            id: doc.key,
+            title: doc.title,
+            genre: doc.genre_name?.[0],
+            director: doc.director_name?.[0],
+            image: `https://bre-wonder-cinema-app-8704977a1a65.herokuapp.com/`,
+          };
+        });
+        setMovies(moviesFromApi);
+      });
+    }, []);
 
   if(selectedMovie) {
     return (
