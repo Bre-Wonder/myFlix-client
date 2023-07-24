4efad0d5bf3b3b1cd6bddfba3ab58eb79b8d3f6c
Bre-Wonder <117392929+Bre-Wonder@users.noreply.github.com>
2023-07-23 17:48:44 -0700

    added api url + added form

--- src/components/signup-view/signup-view.jsx
+++ src/components/signup-view/signup-view.jsx
@@ -17,8 +17,61 @@ export const SignUpView = () => {
       Birthday: birthday
     };
 
-    fetch("")
-  
-  }
+  fetch("https://bre-wonder-cinema-app-8704977a1a65.herokuapp.com/users", {
+    method: "POST"
+    body: JSON.stringify(data),
+    headers: {
+      "Content-Type": "application/json"
+    }
+  }).then((response) => {
+    if (response.ok) {
+      alert("Signup Successful");
+      window.location.reload();
+    } else {
+      alert("Signup Failed");
+    }
+  });
+};
 
-}
+return (
+  <form onSubmit={handleSubmit}>
+    <label>
+      Please Select Username:
+      <input
+        type="text"
+        value={username}
+        onChange={(e) => setUsername(e.target.value)}
+        required
+        minLength="3"
+      />
+    </label>
+    <label>
+      Please Create a Password:
+      <input
+      type="password"
+      value={password}
+      onChange={(e) => setPassword(e.target.value)}
+      required
+      />
+    </label>
+    <label>
+      Email:
+      <input
+      type="email"
+      value={email}
+      onChange={(e) => setEmail(e.target.value)}
+      required
+      />
+    </label>
+    <label>
+      Birthday:
+      <input
+      type="date"
+      value={birthday}
+      onChange={(e) => setBirthday(e.target.value)}
+      required
+      />
+    </label>
+  </form>
+  );
+};
