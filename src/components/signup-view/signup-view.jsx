84e4e386b59059616aa3d7cc0bd79e9067ff6c8d
Bre-Wonder <117392929+Bre-Wonder@users.noreply.github.com>
2023-07-23 17:17:41 -0700

    added signup component

---
+++ src/components/signup-view/signup-view.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import { useState } from "react";
+
+export const SignUpView = () => {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [email, setEmail] = useState("");
+  const [birthday, setBirthday] = useState("");
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    const data = {
+      Username: username,
+      Password: password,
+      Email: email,
+      Birthday: birthday
+    };
+
+    fetch("")
+  
+  }
+
+}
