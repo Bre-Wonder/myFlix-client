2b15de86b3add2846b62cf8648af7dc8fcba62ee
Bre-Wonder <117392929+Bre-Wonder@users.noreply.github.com>
2023-08-03 21:46:44 -0700

    wrapped main-view in container

--- src/index.jsx
+++ src/index.jsx
@@ -1,10 +1,15 @@
 import React from "react";
+import { Container } from "react-bootstrap";
 import { createRoot } from "react-dom/client";
 import { MainView } from "./components/main-view/main-view";
 import "./index.scss";
 
 const myFlixApplication = () => {
-  return <MainView />;
+  return (
+    <Container>
+      <MainView />
+    </Container>
+  );
 };
 
 const container = document.querySelector("#root");
