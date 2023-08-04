cb10f33dc9791ec23d8aeeed6f2cef65b051fa8c
Bre-Wonder <117392929+Bre-Wonder@users.noreply.github.com>
2023-08-03 20:34:12 -0700

    Added form elements from bootstrap

--- src/components/signup-view/signup-view.jsx
+++ src/components/signup-view/signup-view.jsx
@@ -34,45 +34,48 @@ export const SignUpView = () => {
 };
 
 return (
-  <form onSubmit={handleSubmit}>
-    <label>
-      Please Select Username:
-      <input
-        type="text"
-        value={username}
-        onChange={(e) => setUsername(e.target.value)}
+  <Form onSubmit={handleSubmit}>
+      <Form.Group controlId="formUsername">
+        <Form.Label>Username: </Form.Label>
+        <Form.Control
+          type="text"
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+          required
+          minLength="3"
+        />
+    </Form.Group>
+
+    <Form.Group controlId="formPassword">
+      <Form.Label>Password: </Form.Label>
+      <Form.Control
+        type="password"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
         required
-        minLength="3"
       />
-    </label>
-    <label>
-      Please Create a Password:
-      <input
-      type="password"
-      value={password}
-      onChange={(e) => setPassword(e.target.value)}
-      required
-      />
-    </label>
-    <label>
-      Email:
-      <input
-      type="email"
-      value={email}
-      onChange={(e) => setEmail(e.target.value)}
-      required
-      />
-    </label>
-    <label>
-      Birthday:
-      <input
-      type="date"
-      value={birthday}
-      onChange={(e) => setBirthday(e.target.value)}
-      required
-      />
-    </label>
-    <button type="submit">Sign Up</button>
-  </form>
+      </Form.Group>
+
+    <Form.Group controlId="formEmail">
+      <Form.Label>Email: </Form.Label>
+      <Form.Control
+        type="email"
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+        required
+        />
+    </Form.Group>
+
+    <Form.Group controlId="formBirthday">
+      <Form.Label>Birthday: </Form.Label>
+      <Form.Control
+        type="date"
+        value={birthday}
+        onChange={(e) => setBirthday(e.target.value)}
+        required
+        />
+    </Form.Group>
+    <Button varient="primary" type="submit">Sign Up</Button>
+  </Form>
   );
 };
