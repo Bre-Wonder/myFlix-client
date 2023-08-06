49038e9574b08c51ed7b7995219fee64898c82b2
Bre-Wonder <117392929+Bre-Wonder@users.noreply.github.com>
2023-08-05 20:09:44 -0700

    corrected cursor style

--- src/components/movie-view/movie-view.jsx
+++ src/components/movie-view/movie-view.jsx
@@ -22,7 +22,7 @@ export const MovieView = ({ movie, onBackClick }) => {
         <span>Genre: </span>
         <span>{movie.Genre}</span>
       </div>
-      <button onClick={onBackClick} className="back-button" style {{ cursor: "pointer" }}>Back</button>
+      <button onClick={onBackClick} className="back-button" style={{ cursor: "pointer" }}>Back</button>
     </div>
   );
 };
