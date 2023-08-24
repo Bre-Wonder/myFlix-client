cc310568cf8952cd86413088df7995c6c5eca7b8
Bre-Wonder <117392929+Bre-Wonder@users.noreply.github.com>
2023-08-24 14:57:08 -0700

    created profile view file

--- src/components/profile-view/profile-view.jsx
+++ src/components/profile-view/profile-view.jsx
@@ -1,7 +1,19 @@
-import { useReducer } from "react"
 
-export const ProfileView
 
-let favoriteMovies = movies.filter(m =>
-  user.FavoriteMovies.includs(m._id)
-    );
+export const ProfileView = ({user}) => {
+  console.log(user);
+  return (user.Username);
+
+}
+
+// Connect favorite button to ProfileView from MovieCard/Movie View
+//   Be able to add movie
+//   Delete a movie
+
+// Apply Bootstrap
+
+// Get Nav functionalities to work
+
+// let favoriteMovies = movies.filter(m =>
+//   user.FavoriteMovies.includs(m._id)
+//     );
