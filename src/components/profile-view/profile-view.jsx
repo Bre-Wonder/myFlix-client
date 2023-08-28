2b895d60e4e9a458bfc4771d6993b185b7386a0c
Bre-Wonder <117392929+Bre-Wonder@users.noreply.github.com>
2023-08-28 13:48:51 -0700

    updated ProfileView function

--- src/components/profile-view/profile-view.jsx
+++ src/components/profile-view/profile-view.jsx
@@ -1,5 +1,5 @@
 import { useParams } from "react-router";
-import { Card, ListGroup } from "react-bootstrap";
+import { Card } from "react-bootstrap";
 import { MovieCard } from "../movie-card/movie-card";
 
 
@@ -7,7 +7,6 @@ export const ProfileView = ({user}) => {
   const { userId } = useParams();
   console.log(user);
 
-  const user = users.find((b) => b.id === userId);
   return (
     <Card>
       <Card.Body>
@@ -26,16 +25,16 @@ export const ProfileView = ({user}) => {
         </div>
         <div>
           <Card.Text>Birthdate: </Card.Text>
-          <Card.Text tyep="date">{user.Birthday}</Card.Text>
+          <Card.Text type="date">{user.Birthday}</Card.Text>
         </div>
       </Card.Body>
     </Card>
   );
 }
 
-let favoriteMovies = movies.filter(m =>
-  user.FavoriteMovies.includs(m._id)
-  );
+// let favoriteMovies = movies.filter(m =>
+//   user.FavoriteMovies.includs(m._id)
+//   );
 
 // Return User Information 
 // Update User Information 
