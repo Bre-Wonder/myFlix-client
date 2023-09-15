c5bdede1489a7c21faee51c40874f39f01c7b036
Bre-Wonder <117392929+Bre-Wonder@users.noreply.github.com>
2023-09-14 20:40:08 -0700

    took out line breaks + added styling

--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -2,25 +2,28 @@ import React from "react";
 import PropTypes from "prop-types";
 import { Button, Card } from "react-bootstrap";
 import { Link } from "react-router-dom";
+import "./movie-card.scss";
 
 export const MovieCard = ({ movie, user }) => {
   return (
-    <Card className="h-100">
+    <Card className="h-100" style={{ width: '18rem' }}>
       <Card.Img variant="top" src={movie.ImagePath} />
       <Card.Body>
         <Card.Title>{movie.Title}</Card.Title>
         
         <Link to={`/movies/${encodeURIComponent(movie._id)}`}>
-          <Button>Open</Button>
+          <Button className="openButton">Open</Button>
         </Link>
-        <br />
-        <br />
-        <Link 
-        // to={`/users/${encodeURIComponent(user._username)}/movies/${encodeURIComponent(movie._id)}`}
-        >
-          {/* Ask if username needs prop type */}
-          <Button title="Add Movie to Favorites List"> + </Button>
-        </Link>
+        <Button 
+        // onClick={movieFavoriteAdded}
+        className="addButton"
+        varient="primary" type="submit"
+        title="Add Movie to Favorites List"> + </Button>
+        <Button 
+        // onClick={movieFavoriteRemoved}
+        className="removeButton"
+        varient="primary" type="submit"
+        title="Remove Movie from Favorites List"> - </Button>
       </Card.Body>
     </Card> 
   );
