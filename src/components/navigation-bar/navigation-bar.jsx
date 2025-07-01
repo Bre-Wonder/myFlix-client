ece11ceb5c9efc061599e6a94e345230390969d6
Bre-Wonder <117392929+Bre-Wonder@users.noreply.github.com>
2025-07-01 11:24:30 -0700

    added dropdown menus

--- src/components/navigation-bar/navigation-bar.jsx
+++ src/components/navigation-bar/navigation-bar.jsx
@@ -3,7 +3,14 @@ import NavbarToggle from "react-bootstrap/esm/NavbarToggle";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
 import { Link } from "react-router-dom";
-import { MDBInputGroup, MDBInput, MDBIcon, MDBBtn } from 'mdb-react-ui-kit';
+import { 
+  MDBIcon,
+  MDBNavbarItem,
+  MDBBtn,
+  MDBDropdown,
+  MDBDropdownToggle,
+  MDBDropdownMenu,
+  MDBDropdownItem, } from 'mdb-react-ui-kit';
 
 export const NavigationBar = ({ user, onLoggedOut }) => {
   return(
@@ -24,12 +31,47 @@ export const NavigationBar = ({ user, onLoggedOut }) => {
                 <Nav.Link as={Link} to="/">Home</Nav.Link>
                 <Nav.Link as={Link} to="/profile">Profile</Nav.Link>
                 <Nav.Link onClick={onLoggedOut}>Logout</Nav.Link>
-                <MDBInputGroup>
+                <MDBNavbarItem>
+                  <MDBDropdown>
+                    <MDBDropdownToggle tag='a' className='nav-link' role='button'>
+                    Genres
+                    </MDBDropdownToggle>
+                    <MDBDropdownMenu>
+                      <MDBDropdownItem link>Action</MDBDropdownItem>
+                      <MDBDropdownItem link>Romantic Comedy </MDBDropdownItem>
+                      <MDBDropdownItem link>Coming of Age</MDBDropdownItem>
+                      <MDBDropdownItem link>Animation</MDBDropdownItem>
+                      <MDBDropdownItem link>Adventure</MDBDropdownItem>
+                      <MDBDropdownItem link>Superhero</MDBDropdownItem>
+                    </MDBDropdownMenu>
+                  </MDBDropdown>
+                </MDBNavbarItem>
+                <MDBNavbarItem>
+                  <MDBDropdown>
+                    <MDBDropdownToggle tag='a' className='nav-link' role='button'>
+                      Directors
+                    </MDBDropdownToggle>
+                    <MDBDropdownMenu>
+                      <MDBDropdownItem link>Mark Dindal</MDBDropdownItem>
+                      <MDBDropdownItem link>Steven Sordergh</MDBDropdownItem>
+                      <MDBDropdownItem link>Nancy Meyers</MDBDropdownItem>
+                      <MDBDropdownItem link>Kevin Kwapis</MDBDropdownItem>
+                      <MDBDropdownItem link>William Wyler</MDBDropdownItem>
+                      <MDBDropdownItem link>John Musker</MDBDropdownItem>
+                      <MDBDropdownItem link>Taika Waititi</MDBDropdownItem>
+                      <MDBDropdownItem link>Robert Zemeckis</MDBDropdownItem>
+                      <MDBDropdownItem link>Jon M Chu</MDBDropdownItem>
+                    </MDBDropdownMenu>
+                  </MDBDropdown>
+                </MDBNavbarItem>
+
+
+                {/* <MDBInputGroup>
                   <MDBInput placeholder='Search' style={{ borderTopRightRadius: '0', borderBottomRightRadius: '0' }} />
                   <MDBBtn rippleColor='dark' style={{ borderTopLeftRadius: '0', borderBottomLeftRadius: '0' }}>
                     <MDBIcon icon='search' />
                   </MDBBtn>
-                </MDBInputGroup>
+                </MDBInputGroup> */}
               </>
             )}
           </Nav>
